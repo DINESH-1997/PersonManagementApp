b0acddb52520bc5e7e00baada4992dbf02d9dbb4
Dinesh_Santhanakrishnan <dinesh.santhanakrishnan@braveryinfotech.com>
2024-07-19 22:59:45 +0530

    url change

--- app/src/main/java/com/dinesh/personmanagement/common/Constants.kt
+++ app/src/main/java/com/dinesh/personmanagement/common/Constants.kt
@@ -1,5 +1,5 @@
 package com.dinesh.personmanagement.common
 
 object Constants {
-    const val BASE_URL = "https://crudcrud.com/api/93d5dac320e7461584e4b573aedfc8e2/"
+    const val BASE_URL = "https://crudcrud.com/api/bd186b4fd1704200b0cb11897e6f0194/"
 }
